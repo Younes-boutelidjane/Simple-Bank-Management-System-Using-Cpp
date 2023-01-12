cba213456059b645ddd766c58e1036f82fc6e78e
Boutelidjane Younes <80323265+Younes-boutelidjane@users.noreply.github.com>
2023-01-12 14:31:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,24 +6,9 @@
 */
 #include <iostream>
 #include <string>
+#include "Functions.h"
 
-using std::cout;
-using std::cin;
-using std::endl;
-
-struct Account
-{
-	std::string name;
-	int pin_num;
-};
-
-Account client;
 double balance = 0.0;
-
-void showMenu();
-void depositFun();
-void withdrawFunc();
-
 
 int main(){
     
@@ -31,11 +16,11 @@ int main(){
     showMenu();
 do
 {
-   cout << "Selection: ";
-   cin>> selection;
+   std::cout << "Selection: ";
+   std::cin>> selection;
    switch(selection){
         case 1:
-        cout<<"Your balance is: $"<<balance<<"\n\n";
+        std::cout<<"Your balance is: $"<<balance<<"\n\n";
         break;
         case 2:
         depositFun();
@@ -44,45 +29,15 @@ do
         withdrawFunc();
         break;
         case 4:
-		cout << "Thank you and Have a nice day :)";
+		std::cout << "Thank you and Have a nice day :)";
         exit(1);
         break;
         default:
-        cout << "Invalid selection !!, enter a value between 1 & 4\n\n";
+        std::cout << "Invalid selection !!, enter a value between 1 & 4\n\n";
         break;}
 } while (selection != 4);
 
     system("pause");
 
     return 0;
-}
-void showMenu(){
-    cout<<"*************Hello!*************\n";
-    cout<<"******Welcome to ATM banking******\n\n";
-	cout<<"Please enter you name and your pin number\n";
-    cout<<"Name: ";
-    cin >> client.name;
-    cout<<"\npin number: ";
-    cin >> client.pin_num;
-    cout << "Welcome "<< client.name <<"\nHow can I help you today?\n\n";
-    cout<<"**Please choose one of the option below**\n\n";
-    cout<<"< 1 > Check balance\n";
-    cout<<"< 2 > Deposit\n";
-    cout<<"< 3 > Withdraw\n";
-    cout<<"< 4 > Exit\n\n\n";
-}
-void depositFun(){
-    double depositAmount = 0.0;
-    cout << "Deposit Amount : ";
-    cin >> depositAmount;
-    balance += depositAmount;
-}
-void withdrawFunc(){
-    double withdrawAmount = 0.0;
-    cout << "Withdraw Amount: $";
-    cin >> withdrawAmount;
-    if(withdrawAmount <= balance)
-        balance -= withdrawAmount;
-    else
-        cout << "Not enough money \n\n ";
 }
